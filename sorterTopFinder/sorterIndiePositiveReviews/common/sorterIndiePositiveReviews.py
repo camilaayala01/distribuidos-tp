dd0060e6a56a359987221d3f4ddc900301cfbe24
Noah Masri <mmasri@fi.uba.ar>
2024-09-27 20:20:14 -0300

    finished refactoring. gotta delete unnecessary or repeated tests

--- sorterTopFinder/sorterIndiePositiveReviews/common/sorterIndiePositiveReviews.py
+++ sorterTopFinder/sorterIndiePositiveReviews/common/sorterIndiePositiveReviews.py
@@ -5,4 +5,4 @@ TOP_AMOUNT = 5
 
 class SorterIndiePositiveReviews(SorterTopFinder):
     def __init__(self, topAmount: int = TOP_AMOUNT): # for testing purposes
-        super().__init__("SorterPlaytime", EntryNameReviewCount, topAmount)
+        super().__init__("SorterReviewCount", EntryNameReviewCount, topAmount)
