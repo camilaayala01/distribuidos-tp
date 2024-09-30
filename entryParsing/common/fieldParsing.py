a77a4c088364a5d37b05eef17b4a71dab0aa0ee2
Noah Masri <mmasri@fi.uba.ar>
2024-09-30 16:35:35 -0300

    refactor entries in sorter

--- entryParsing/common/fieldParsing.py
+++ entryParsing/common/fieldParsing.py
@@ -3,6 +3,7 @@ from .utils import boolToInt, intToBool
 
 STRING_LEN = 1
 COUNT_LEN = 4
+AVG_PLAYTIME_LEN = 4
 BOOLEAN_BYTES = 1
 
 def serializeVariableLenString(field: str):
@@ -16,7 +17,6 @@ def deserializeVariableLenString(curr: int, data: bytes)-> Tuple[str, int]:
     appID = data[curr:fieldLen+curr].decode()
     return appID, curr + fieldLen
 
-
 def serializeCount(count: int):
     return count.to_bytes(COUNT_LEN,'big')
 
@@ -29,3 +29,10 @@ def serializeBoolean(os: bool):
 
 def deserializeBoolean(curr: int, data: bytes)-> Tuple[bool, int]:
     return intToBool(int.from_bytes(data[curr:curr+BOOLEAN_BYTES], 'big')), curr + BOOLEAN_BYTES
+
+def serializePlaytime(avgPlaytime: int)-> Tuple[int, int]:
+    return avgPlaytime.to_bytes(AVG_PLAYTIME_LEN,'big')
+
+def deserializePlaytime(curr: int, data: bytes)-> Tuple[int, int]:
+    avgPlaytime = int.from_bytes(data[curr:curr + AVG_PLAYTIME_LEN], 'big')
+    return avgPlaytime, curr + AVG_PLAYTIME_LEN
--- sorterTopFinder/sorterByAvgPlaytime/common/entryNameAvgPlaytime.py
+++ sorterTopFinder/sorterByAvgPlaytime/common/entryNameAvgPlaytime.py
@@ -1,8 +1,6 @@
-# amount of bytes dedicated to stating the length of the name
 from typing import Tuple
+from entryParsing.common.fieldParsing import deserializePlaytime, deserializeVariableLenString, serializePlaytime, serializeVariableLenString
 from sorterTopFinder.common.entrySorterTopFinder import EntrySorterTopFinder
-
-AVG_PLAYTIME_LEN = 4
 
 class EntryNameAvgPlaytime(EntrySorterTopFinder):
     def __init__(self, name: str, avgPlaytime: int):
@@ -10,21 +8,20 @@ class EntryNameAvgPlaytime(EntrySorterTopFinder):
         self._avgPlaytime = avgPlaytime
 
     def serialize(self) -> bytes:
-        baseSerialized = super().serialize()
-        avgPlaytimeBytes = self._avgPlaytime.to_bytes(AVG_PLAYTIME_LEN, 'big')
+        nameBytes = serializeVariableLenString(self._name)
+        avgPlaytimeBytes = serializePlaytime(self._avgPlaytime)
 
-        return baseSerialized + avgPlaytimeBytes   
+        return nameBytes + avgPlaytimeBytes   
 
     def __str__(self):
         return f"EntryNameAvgPlaytime(name={self._name}, avgPlaytime={self._avgPlaytime})"
     
     @classmethod
     def deserializeEntry(cls, curr: int, data: bytes) -> Tuple['EntryNameAvgPlaytime', int]:
-        curr, name = super().deserializeName(curr, data)
-        reviewCount = int.from_bytes(data[curr:curr + AVG_PLAYTIME_LEN], 'big')
-        curr += AVG_PLAYTIME_LEN
+        name, curr = deserializeVariableLenString(curr, data)
+        avgPlaytime, curr = deserializePlaytime(curr, data)
 
-        return cls(name, reviewCount), curr
+        return cls(name, avgPlaytime), curr
     
     def getSortingAtribute(self) -> int:
         return self._avgPlaytime
