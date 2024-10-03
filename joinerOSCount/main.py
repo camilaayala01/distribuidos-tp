d64ba0868c39fd4d1892fb264945071be3e43c2a
Noah Masri <mmasri@fi.uba.ar>
2024-10-03 15:48:20 -0300

    add execute to main

--- joinerOSCount/main.py
+++ joinerOSCount/main.py
@@ -2,7 +2,7 @@ from .common.joinerOSCount import JoinerOSCount
 
 def main():
     joiner = JoinerOSCount()
-    print("initialized joiner")
+    joiner.execute()
 
 if __name__ == "__main__":
     main()
