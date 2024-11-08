95b97a7437882703a36a76bc7cdb596eb51e2360
Noah Masri <mmasri@fi.uba.ar>
2024-11-08 16:46:39 -0300

    add volumes and try the file write

--- packetTracker/packetTracker.py
+++ packetTracker/packetTracker.py
@@ -1,3 +1,4 @@
+import os
 from entryParsing.common.header import Header
 from packetTracker.tracker import TrackerInterface
 
@@ -5,10 +6,15 @@ class PacketTracker(TrackerInterface):
     def __init__(self, nodesInCluster: int, module: int, storagePath: str):
         self._nodesInCluster = nodesInCluster
         self._module = module
-        self._biggestFragment = 0
+        self._biggestFragment = 0 #escribir
         self._pending = set()
         self._receivedEnd = False
-        self._storagePath = storagePath
+        self._storagePath = f"/packetTracker/{storagePath}.txt"
+        os.makedirs(f"/packetTracker", exist_ok=True)
+
+    def store(self):
+        with open(self._storagePath, 'w+') as file:
+            file.write(f"MAX={self._biggestFragment};MISSING={self._pending}")
 
     def isDuplicate(self, header: Header):
         newFrag = header.getFragmentNumber()
@@ -20,12 +26,14 @@ class PacketTracker(TrackerInterface):
         if newFrag > self._biggestFragment:
             for num in range (self._biggestFragment + 1, newFrag):
                 if num % self._nodesInCluster == self._module:
-                    self._pending.add(num)
+                    self._pending.add(num) 
 
             self._biggestFragment = newFrag
             self._receivedEnd = header.isEOF()
         else:
             self._pending.discard(newFrag)
+        
+        self.store()
 
     def isDone(self):
         return len(self._pending) == 0 and self._receivedEnd
