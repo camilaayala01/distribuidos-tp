11e23054dc682df7b4594c7cfdc75798b859e867
Noah Masri <mmasri@fi.uba.ar>
2024-11-26 14:58:52 -0300

    add messages file

--- healthcheckAnswerController/healthcheckAnswerController.py
+++ healthcheckAnswerController/healthcheckAnswerController.py
@@ -1,6 +1,8 @@
 import socket
 import os
-import threading 
+import threading
+
+from .messages import HeartbeatMessage 
 
 def sendall(msg, addr, sock):
     sent = 0
@@ -11,19 +13,23 @@ class HealthcheckAnswerController:
     def __init__(self):
         self._nodeName = os.getenv('NODE_NAME')
         self._running = True
-        self._healthcheckPort = int(os.getenv('HEALTHCHECK_PORT')) 
+        self._healthcheckPort = int(os.getenv('HEALTHCHECK_PORT'))
+        self._handle = None 
+    
+    def isRunning(self):
+        return self._running
     
     def setUpHealthcheck(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         print(f'escucho en {self._nodeName}')
         sock.bind((f'{self._nodeName}', self._healthcheckPort))
         sock.settimeout(2)
-        while self._running:
+        while self.isRunning(): 
             try:
                 data, addr = sock.recvfrom(1024)
-                if data.decode('utf-8') == "check":
-                    msg = f"ack:{self._nodeName}".encode('utf-8')
-                    sendall(msg, addr, sock)
+                msg, _ = HeartbeatMessage.deserialize(data)    
+                if msg == HeartbeatMessage.CHECK:
+                    sendall(HeartbeatMessage.ACK.serialize(self._nodeName), addr, sock)
             except TimeoutError:
                 continue
         sock.close()
@@ -31,8 +37,13 @@ class HealthcheckAnswerController:
     def stop(self):
         print("dejo de correr")
         self._running = False
+        if self._handle:
+            self._handle.join()
 
     def execute(self):
-        threading.Thread(target=self.setUpHealthcheck).start()
+        healthcheckThread = threading.Thread(target=self.setUpHealthcheck)
+        self._handle = healthcheckThread
+        healthcheckThread.start()
         print("levanto el healthcheck y eso gordi")
+
 
--- monitor/main.py
+++ monitor/main.py
@@ -1,12 +1,13 @@
 import signal
 from monitor import Monitor
+import os
 
-
+TIMER_DURATION = int(os.getenv('TIMER_DURATION'))
 def main():
     monitor = Monitor()
     signal.signal(signal.SIGTERM, monitor.stop)
     signal.signal(signal.SIGALRM, monitor.checkStatus)
-    signal.setitimer(signal.ITIMER_REAL, 3, 3)
+    signal.setitimer(signal.ITIMER_REAL, TIMER_DURATION, TIMER_DURATION)
     monitor.run()
     
 if __name__ == "__main__":
